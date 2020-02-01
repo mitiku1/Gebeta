d11b42a3bf262d957c3636763fdfef3899145cb2
Mitiku Yohannes <se.mitiku.yohannes@gmail.com>
2020-02-01 13:28:33 +0200

    add gitignore file, add count moves

--- count_states.py
+++ count_states.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def main():
     env = gym.make("Gebeta-v0")
-    states_count = defaultdict(int)
+    states_count = set()
 
-    max_iteration = 100000
-
+    max_iteration = 10000
+    num_moves = []
     for i in range(max_iteration):
         state = env.reset()
         done = False
@@ -19,11 +19,12 @@ def main():
             action = np.random.choice(6)
             start, reward, done, info = env.step(action, player)
             player = (player+1)%2
-            states_count[tuple(state)] += 1
+            states_count.add(tuple(state))
             game_iteration += 1
-        if (i+1) %100==0:
+        num_moves.append(game_iteration)
+        if (i+1) %1000==0:
             print("Iteration: {} Number of states: {}".format(i+1, len(states_count)))
-            
+    print("Avg moves:",sum(num_moves)/len(num_moves))
 
 
 if __name__=='__main__':
